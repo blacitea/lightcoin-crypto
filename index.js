efdab3f54e3080d34a89d710d5389acdbbdc406f
Angel Tsang <blacitea@gmail.com>
2020-08-01 01:06:35 +0000

    using get set

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
-let balance = 500.00;
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    // no money in new account, yet
+    this.transactions = [];
+  }
+
+  get balance() {
+    let balance = 0;
+    for (let event of this.transactions) {
+      balance += event.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
 
 class Transaction { // I am a SUPER class
+
   constructor(amount, account) {
     this.amount = amount;
     this.account = account;
+  }
+
+  commit() {
+    this.time = new Date();
+    this.account._balance += this.value;
+    this.account.addTransaction(this);
   }
 }
 
 class Withdrawal extends Transaction {
 
-  commit() {
-    this.account.balance -= this.amount;
+  get value() {
+    return (- this.amount);
   }
 
 }
 
 class Deposit extends Transaction {
 
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
   }
 }
 
-class Account {
 
-  constructor(username) {
-    this.username = username;
-    // no money in new account, yet
-    this.balance = 0;
-  }
-}
 
 
 // DRIVER CODE BELOW
@@ -39,17 +58,17 @@ class Account {
 const myAccount = new Account("snow-patrol");
 console.log(myAccount);
 
-t1 = new Withdrawal(50.25, myAccount);
+let t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99, myAccount);
+let t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00, myAccount);
+let t3 = new Deposit(120.00, myAccount);
 t3.commit();
 console.log('Transaction 3:', t3);
 
-console.log('Balance:', myAccount.balance);
+console.log('Ending Balance:', myAccount.balance);
 
